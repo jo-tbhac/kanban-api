506b276e08ec4cc55b1a0423d4bbb8ec6e16faef
Jun <cornerstone.am3@gmail.com>
2020-05-06 04:21:52 +0900

    set up db connection

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/jo-tbhac/kanban-api/config"
+	"github.com/jo-tbhac/kanban-api/db"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	db := db.Get()
+	defer db.Close()
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run()
+	r.Run(fmt.Sprintf(":%v", config.Config.Web.Port))
 }
