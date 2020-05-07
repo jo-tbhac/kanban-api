cee9027057042ee786da038af5f746d5d04658c5
Jun <cornerstone.am3@gmail.com>
2020-05-07 18:44:07 +0900

    add logging if environment on development

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/jo-tbhac/kanban-api/controllers"
 	"github.com/jo-tbhac/kanban-api/db"
 )
@@ -8,6 +10,10 @@ import (
 func main() {
 	db := db.Get()
 	defer db.Close()
+
+	if os.Getenv("GIN_MODE") == "debug" {
+		db.LogMode(true)
+	}
 
 	controllers.StartServer()
 }
