04905918f9fe8705d58786fab8c4fd7498c75a38
Jun <cornerstone.am3@gmail.com>
2020-06-01 17:21:38 +0900

    add test for fail to create user

--- repository/user_test.go
+++ repository/user_test.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"fmt"
 	"regexp"
 	"testing"
 
@@ -40,4 +41,37 @@ func TestShouldSuccessfullyCreateUser(t *testing.T) {
 	assert.Equal(t, u.Name, name)
 	assert.Equal(t, u.Email, email)
 	assert.Equal(t, u.PasswordDigest, password)
+}
+
+func TestShouldNotCreateUserWhenDuplicateEmail(t *testing.T) {
+	db, mock := utils.NewDBMock(t)
+	defer db.Close()
+
+	r := NewUserRepository(db)
+
+	name := "gopher"
+	email := "gopher@sample.com"
+	password := "password"
+	createdAt := utils.AnyTime{}
+	updatedAt := utils.AnyTime{}
+
+	mock.ExpectBegin()
+	mock.ExpectExec(regexp.QuoteMeta(
+		"INSERT INTO `users` (`created_at`,`updated_at`,`name`,`email`,`password_digest`,`remember_token`)")).
+		WithArgs(createdAt, updatedAt, name, email, password, "").
+		WillReturnError(fmt.Errorf("Error 1062: Duplicate entry '%s' for key 'email'", email))
+
+	mock.ExpectRollback()
+
+	_, err := r.Create(name, email, password)
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %v", err)
+	}
+
+	if err == nil {
+		t.Error("there were unfulfilled expectations: it did not recieve error")
+	}
+
+	assert.Equal(t, err[0].Text, fmt.Sprintf("%s has already been taken", email))
 }
