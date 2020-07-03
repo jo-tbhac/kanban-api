17c93e359bee375d2e683bafd8fc1395ad1644bf
Jun <cornerstone.am3@gmail.com>
2020-07-03 15:11:07 +0900

    add test for update list index

--- handler/list_test.go
+++ handler/list_test.go
@@ -47,7 +47,7 @@ func TestCreateListHandlerShouldReturnsStatusCreatedWithListData(t *testing.T) {
 
 	mock.ExpectQuery(regexp.QuoteMeta("SELECT user_id FROM `boards`"))
 	mock.ExpectBegin()
-	mock.ExpectExec(regexp.QuoteMeta("INSERT INTO `lists` (`created_at`,`updated_at`,`deleted_at`,`name`,`board_id`)")).
+	mock.ExpectExec(regexp.QuoteMeta("INSERT INTO `lists`")).
 		WillReturnResult(sqlmock.NewResult(1, 1))
 
 	mock.ExpectCommit()
@@ -202,6 +202,46 @@ func TestShouldFailureUpdateListHandlerWhenWithoutName(t *testing.T) {
 
 	assert.Equal(t, w.Code, 400)
 	assert.Equal(t, res["errors"][0].Text, "Name must exist")
+}
+
+func TestUpdateListIndexShouldReturnsStatusOK(t *testing.T) {
+	db, mock := utils.NewDBMock(t)
+	defer db.Close()
+
+	lh := NewListHandler(repository.NewListRepository(db))
+	uh := NewUserHandler(repository.NewUserRepository(db))
+
+	r := utils.SetUpRouter()
+
+	b, err := json.Marshal([]struct {
+		ID    uint
+		Index int
+	}{
+		{ID: 1, Index: 1},
+		{ID: 2, Index: 3},
+		{ID: 3, Index: 2},
+	})
+
+	if err != nil {
+		t.Fatalf("fail to marshal json: %v", err)
+	}
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodPatch, "/lists/index", bytes.NewReader(b))
+
+	utils.SetUpAuthentication(r, req, mock, uh.Authenticate(), MapIDParamsToContext())
+
+	mock.ExpectExec(regexp.QuoteMeta("UPDATE `lists` SET `index` = ELT(FIELD(id,1,2,3),1,3,2) WHERE id IN (1,2,3)")).
+		WillReturnResult(sqlmock.NewResult(1, 3))
+
+	r.PATCH("/lists/index", lh.UpdateListIndex)
+	r.ServeHTTP(w, req)
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Fatalf("there were unfulfilled expectations: %v", err)
+	}
+
+	assert.Equal(t, w.Code, 200)
 }
 
 func TestDeleteListShouldReturnsStatusOK(t *testing.T) {
