d2708001a6c81ed27c1380a812f9c4a9a1b631c9
Jun <cornerstone.am3@gmail.com>
2020-08-06 11:36:24 +0900

    create entity 'Cover'

---
+++ entity/cover.go
@@ -0,0 +1,7 @@
+package entity
+
+// Cover is model of covers table.
+type Cover struct {
+	CardID uint `json:"card_id" gorm:"primary_key;auto_increment:false"`
+	FileID uint `json:"file_id" gorm:"primary_key;auto_increment:false"`
+}
